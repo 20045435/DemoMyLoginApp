3f736d5571fbb75707c33b16ebcda48a3ba7d7e6
FAITH SIM MIN HUI <20045435@myrp.edu.sg>
2021-04-28 19:29:02 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20045435/demomyloginapp/MainActivityS2Ex2.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20045435.demomyloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityS2Ex2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_s2_ex2);
+    }
+}
